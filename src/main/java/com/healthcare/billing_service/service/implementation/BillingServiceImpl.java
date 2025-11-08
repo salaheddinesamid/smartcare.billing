fd5bb521abaaac7633519b817d3d52ee0a7ce0f4
salaheddinesamid <salaheddine.kobra@iu-study.org>
2025-11-08 10:28:11 +0100

    update

--- src/main/java/com/healthcare/billing_service/service/implementation/BillingServiceImpl.java
+++ src/main/java/com/healthcare/billing_service/service/implementation/BillingServiceImpl.java
@@ -7,27 +7,41 @@ import com.healthcare.billing_service.model.Invoice;
 import com.healthcare.billing_service.model.InvoiceStatus;
 import com.healthcare.billing_service.repository.InvoiceRepository;
 import com.healthcare.billing_service.service.BillingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
 
 @Service
 public class BillingServiceImpl implements BillingService {
 
-    private final InvoiceRepository invoiceRepository;
+    @Value("${services.patient}")
+    private String PATIENT_SERVICE;
 
-    public BillingServiceImpl(InvoiceRepository invoiceRepository) {
+    private final InvoiceRepository invoiceRepository;
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public BillingServiceImpl(InvoiceRepository invoiceRepository, RestTemplate restTemplate) {
         this.invoiceRepository = invoiceRepository;
+        this.restTemplate = restTemplate;
     }
 
     @Override
     public NewInvoiceResponseDto generateInvoice(Integer appointmentId, Integer patientId, NewInvoiceDto requestDto) {
+        /*
         // Check patient existence:
         boolean patientExists = patientExistence(patientId);
 
         if(!patientExists){
             // throw exception:
         }
+
+         */
+
+
 
         Invoice invoice = new Invoice();
         invoice.setAmount(requestDto.getAmount());
@@ -46,9 +60,15 @@ public class BillingServiceImpl implements BillingService {
 
     }
 
+    /*
+    // This function communicates with patient-management service to verify patient existence:
     private boolean patientExistence(Integer patientId){
-        return true;
+        String uri = String.format("%s%s",PATIENT_SERVICE,"/api/patient-management/verify-existence")
+        ResponseEntity<?> response =
+                restTemplate.exchange()
     }
+
+     */
 
 
     @Override
